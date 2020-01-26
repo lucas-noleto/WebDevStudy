3e4133134d838acc12299c5fb24fe65ccdbb21f2
Lucas Noleto <lucas_noleto@outlook.com>
2020-01-26 10:27:04 -0300

    Upload de arquivos já feitos e Aulas 17 ~ 27.

---
+++ exercicios/Fundamentos/21.TemplateString.js
@@ -0,0 +1,10 @@
+const name = 'Rebeca'
+const concatenacao = 'Ola' + name +' !'
+console.log(concatenacao)
+const template = `
+    Olá 
+    ${name}!
+`
+console.log(template)
+console.log(`1+1 = ${1+1}` )
+
