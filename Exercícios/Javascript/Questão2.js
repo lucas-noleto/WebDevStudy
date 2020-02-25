bcb7384f259f0a28171af727b836268435e65997
Lucas Noleto <lucas_noleto@outlook.com>
2020-02-25 07:43:36 -0300

    Adicionado exercicios..

---
+++ Exercícios/Javascript/Questão2.js
@@ -0,0 +1,7 @@
+function expoente(base,expoente){
+    x = Math.pow( base,expoente)
+    return console.log(`${x}`)
+}
+
+
+expoente(2,8)
---
+++ Exercícios/Questão1.js
@@ -0,0 +1,12 @@
+function calculadora (x,y){
+    soma = x + y
+    sub = x - y
+    mult = x * y
+
+    return console.log(
+    `Soma: ${soma}
+    Subtração: ${sub}      
+    Multiplicação: ${mult}` )
+}
+
+calculadora(4,2)
